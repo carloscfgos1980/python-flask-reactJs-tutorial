39e535a828bdf389bdf3f15e583854edadc49054
Carlos Infante <kuva5008@gmail.com>
2023-02-27 05:00:00 +0100

    post method ok

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_marshmallow import Marshmallow
 
 
+app = Flask(__name__)
 DB_NAME = "flask.db"
 db = SQLAlchemy()
 
-app = Flask(__name__)
+ma = Marshmallow(app)
+
 app.config['SECRET_KEY'] = 'myKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 db.init_app(app)
@@ -23,6 +26,15 @@ class Articles(db.Model):
         self.body = body
 
 
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+
 with app.app_context():
     db.create_all()
 
@@ -30,6 +42,17 @@ with app.app_context():
 @app.route('/', methods=['GET'])
 def get_articles():
     return jsonify({"Hello": "World"})
+
+
+@app.route('/add', methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
 
 
 if __name__ == "__main__":
---
+++ inst3.md
@@ -0,0 +1,28 @@
+# Post method
+
+1. Import modules
+from flask import Flask, jsonify, request
+from flask_marshmallow import Marshmallow
+
+2. Call the Marshmallow function:
+ma = Marshmallow(app)
+
+3. Create the class for the articles format:
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+4. Call the function (class) <ArticleSchema> and set the config:
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+5. Add another route for the post:
+@app.route('/add', methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
